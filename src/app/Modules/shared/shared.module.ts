97706f3a345baa1e4baa4dfe06e0cd2099204d29
ahmedelborgy <ahmedelborgy130@gmail.com>
2024-03-20 00:19:00 +0200

    shared header

--- src/app/Modules/shared/shared.module.ts
+++ src/app/Modules/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule
