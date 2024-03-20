9ac780a25913bb55c3064a28b463a0f547f42c16
ayamedhat229 <ayamedhat773@gmail.com>
2024-03-20 20:59:21 +0200

    integrateApis

--- src/app/Modules/dashboard/dashboard.module.ts
+++ src/app/Modules/dashboard/dashboard.module.ts
@@ -4,16 +4,23 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class DashboardModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './Modules/guards/auth.guard';
 
 const routes: Routes = [
 
@@ -10,7 +11,7 @@ const routes: Routes = [
 
   
   
-  { path: 'auth', loadChildren: () => import('./Modules/auth/auth.module').then(m => m.AuthModule) }
+  { path: 'auth',canActivate:[AuthGuard], loadChildren: () => import('./Modules/auth/auth.module').then(m => m.AuthModule) }
   
   
   
