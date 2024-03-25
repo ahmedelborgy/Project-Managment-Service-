1bcaa12115a7fa595a7975f29d9d1672d7952a4f
AhmedeElborgy <ahmedelborgy140@gmail.com>
2024-03-25 07:02:59 +0200

    change password

--- src/app/Modules/dashboard/navbar/navbar.component.ts
+++ src/app/Modules/dashboard/navbar/navbar.component.ts
@@ -1,18 +1,69 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/service/auth.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ChangePasswordComponent } from '../../auth/component/change-password/change-password.component';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  constructor(private _AuthService:AuthService){}
+  constructor(private _AuthService:AuthService,
+    private _Router:Router,
+    public dialog: MatDialog
+    ){}
   ngOnInit(): void {
     
   }
   userName=localStorage.getItem('userName');
  // email=localStorage.getItem('Email')
   myLogout(){
-    this._AuthService.logOut()
+    this._AuthService.logOut();
   }
+
+
+  
+  openDialogChangePass(): void {
+    const dialogRef = this.dialog.open(ChangePasswordComponent, {
+      data: {name},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed',result);
+     if(result!=undefined){
+      this.onChangePassw(result);
+     }
+    });
+  }
+
+   onChangePassw(changeForm:any){
+    console.log(changeForm);
+    
+    console.log(changeForm);
+    
+    this._AuthService.ChangePassword(changeForm).subscribe({
+      next:(res)=>{
+        console.log(res)
+      },
+      error:(err)=>{
+        console.log(err)
+      },
+      complete:()=>{
+  this._Router.navigate(['/auth/login'])
+      }
+    })
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
+
