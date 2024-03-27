03e847b3f7854b0e4346132931ee1edb29f759ae
ayamedhat229 <ayamedhat773@gmail.com>
2024-03-27 06:06:11 +0200

    test

---
+++ src/app/Modules/dashboard/Modules/employe/service/employee-tasks.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeTasksService {
+
+  constructor(private _HttpClient:HttpClient){}
+  getAllTasks(params:any):Observable<any>{
+    return this._HttpClient.get('Task',{params:params})
+  }
+}
+
--- src/app/Modules/dashboard/Modules/employe/tasks/tasks-user/tasks-user/tasks-user.component.ts
+++ src/app/Modules/dashboard/Modules/employe/tasks/tasks-user/tasks-user/tasks-user.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { EmployeeTasksService } from '../../../service/employee-tasks.service';
 @Component({
   selector: 'app-tasks-user',
   templateUrl: './tasks-user.component.html',
   styleUrls: ['./tasks-user.component.scss']
 })
-export class TasksUserComponent {
+export class TasksUserComponent implements OnInit {
+  constructor(private _EmployeeTasksService:EmployeeTasksService){}
+  ngOnInit(): void {
+    this.openAllTasks()
+  }
+  todo:any[]=[]
+  inPrograss:any[]=[]
+  Done:any[]=[]
+  taskData:any[]=[]
+  taskList:any[]=[]
+  openAllTasks(){
+    let params={
+      
+    }
+    this._EmployeeTasksService.getAllTasks(params).subscribe({
+      next:(res)=>{
+        console.log(res)
+        this.taskData=res;
+        this.taskList=res.data;
+        this.todo=this.taskList.filter(x=>x.status=='ToDo');
+       
+        console.log(this.todo)
+        this.inPrograss=this.taskList.filter(x=>x.status=='InProgress')
+        this.Done=this.taskList.filter(x=>x.status=='Done')
+      }
+    })
+  }
 
 }
